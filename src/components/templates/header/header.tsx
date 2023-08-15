ffbffef1f0ea240511c5f350d3652b3ab1b17a29
krontill <till.work@gmail.com>
2023-08-15 22:38:21 +0200

    Hide LanguageSelector

--- src/components/templates/header/header.tsx
+++ src/components/templates/header/header.tsx
@@ -2,7 +2,7 @@ import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
 
 import BlogLogo from '@icons/blog-logo.svg';
-import { LanguageSelector } from '@src/components/features/language-selector';
+// import { LanguageSelector } from '@src/components/features/language-selector';
 import { Container } from '@src/components/shared/container';
 
 export const Header = () => {
@@ -15,7 +15,7 @@ export const Header = () => {
           <Link href="/" title={t('common.homepage')}>
             <BlogLogo />
           </Link>
-          <LanguageSelector />
+          {/*<LanguageSelector />*/}
         </Container>
       </nav>
     </header>
