72273d2a83ff26a6f4a456a1f1ee13479c434884
krontill <till.work@gmail.com>
2023-08-20 22:24:09 +0200

    Add googletagmanager

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -1,9 +1,20 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <Script
+          id="show-banner"
+          dangerouslySetInnerHTML={{
+            __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P86GPW35');</script>`,
+          }}
+        />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
@@ -15,6 +26,15 @@ export default function Document() {
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
+        <noscript>
+          <iframe
+            title="googletagmanager"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P86GPW35"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
