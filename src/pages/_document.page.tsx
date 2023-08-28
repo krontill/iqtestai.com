69b0f734900b678cbead80e9ff4bf47be9077414
krontill <till.work@gmail.com>
2023-08-28 20:47:22 +0200

    Google-analytics test

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -15,16 +15,6 @@ export default function Document() {
             gtag('config', 'G-B0S6ZDL6QL');
           `}
         </Script>
-        <Script
-          id="googletagmanager"
-          dangerouslySetInnerHTML={{
-            __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P86GPW35');</script>`,
-          }}
-        />
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
@@ -36,15 +26,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <body>
-        <noscript>
-          <iframe
-            title="googletagmanager"
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P86GPW35"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         <Main />
         <NextScript />
       </body>
